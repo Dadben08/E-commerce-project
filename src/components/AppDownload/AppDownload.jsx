97d44ac23a56939b16cdd9d4aadc9dd838a99ffe
Dadben08 <Dadben08@gmail.comcleargit config --global user.email Dadben08@gmail.com>
2024-09-17 17:21:01 +0100

    tomato

--- src/components/AppDownload/AppDownload.jsx
+++ src/components/AppDownload/AppDownload.jsx
@@ -5,7 +5,7 @@ import { assets } from '../../assets/assets'
 const AppDownload = () => {
   return (
     <div className='App-download' id='App-download'>
-    <p>For Better Experience Download <br />Tomato App</p>
+    <p>For Better Experience Download <br />Cravings App</p>
     <div className="app-download-platforms">
         <img src={assets.play_store} alt="" />
         <img src={assets.app_store} alt="" />
